f350198483e8a1c0fb2e7378fb69ae02c7fd6326
baejinnwoo <bjwkor@naver.com>
2020-02-17 23:08:14 +0900

    작업

--- src/Routes/Search.js
+++ src/Routes/Search.js
@@ -1,1 +1,17 @@
-export default () => "Search";
+import React from "react";
+import {gql, split} from "apollo-boost";
+import {withRouter} from "react-router-dom"
+import styled from "styled-components";
+const Wrapper = styled.div`
+    height:50vh;
+`;
+
+export default withRouter(({location:{search}}) => {
+   const searchTerm = search.split("=")[1];
+   if(searchTerm === undefined){
+       console.log("no Search");}
+    return (
+    <Wrapper>
+        {searchTerm===undefined }
+    </Wrapper>);
+});
